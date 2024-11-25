8d8ad16db4f0e7fff2b64e9970fb803bbd4b0f46
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-11-25 17:31:15 +0000

    save final

--- geradorSenhas/App.js
+++ geradorSenhas/App.js
@@ -2,57 +2,59 @@ import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-na
 import { useState } from 'react';
 import { ModalPassword } from './src/components/modal';
 
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SavedPasswords from './src/screens/SavedPasswords';
 
-let charset = "abcdefghijklmnopqrstuvwxyz!#$&%0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let charset = "abcdefghijklmnopqrstuvwxyz!#$&%0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const Stack = createStackNavigator();
 
 function HomeScreen({ navigation }) {
-  const [senhaGerada, setSenhaGerada] = useState("")
-  const [modalVisible, setModalVisible] = useState(false)
+  const [senhaGerada, setSenhaGerada] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
   const [savedPasswords, setSavedPassword] = useState([]);
+
 
   function gerarSenha() {
     let senha = "";
 
     for (let i = 0, n = charset.length; i < 10; i++) {
-      senha += charset.charAt(Math.floor(Math.random() * n))
+      senha += charset.charAt(Math.floor(Math.random() * n));
     }
-    setSenhaGerada(senha)
-    setModalVisible(true)
-
+    setSenhaGerada(senha);
+    setModalVisible(true);
   }
 
   function salvarSenha() {
-    setSavedPasswords(prevPasswords => {
-      const updatedPasswords = [...prevPasswords, senhaGerada];
-      setModalVisible(false); // Fecha o modal após salvar a senha
-      navigation.navigate('SavedPasswords', { savedPasswords: updatedPasswords }); // Navega e passa as senhas
-      return updatedPasswords; // Atualiza o estado de senhas salvas
-    });
-
-
-    return (
-      <View style={styles.container}>
-        <Image
-          source={require("./src/img/logolock.png")}
-          style={styles.logo}
-        />
-        <Text style={styles.title}>LockGen</Text>
-        <TouchableOpacity style={styles.button} onPress={gerarSenha}>
-          <Text style={styles.textButton}>Gerar Senha</Text>
-        </TouchableOpacity>
-        <Modal visible={modalVisible} animationType='fade' transparent={true}>
-          <ModalPassword senha={senhaGerada} fecharModal={() => setModalVisible(false)} salvarSenha={salvarSenha} />
-        </Modal>
-      </View>
-    );
+    const updatedPasswords = [...savedPasswords, senhaGerada];
+    setSavedPassword(updatedPasswords);
+    setModalVisible(false); // Close modal after saving password
+    navigation.navigate('SavedPasswords', { savedPasswords: updatedPasswords }); // Navigate with updated passwords
   }
+
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("./src/img/download.png")} style={styles.logo} />
+
+      <Text style={styles.title}>Segurança Total</Text>
+      <TouchableOpacity style={styles.button} onPress={gerarSenha}>
+        <Text style={styles.textButton}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+
+
+      <Modal visible={modalVisible} animationType='fade' transparent={true}>
+        <ModalPassword
+          senha={senhaGerada}
+          fecharModal={() => setModalVisible(false)}
+          salvarSenha={salvarSenha}
+        />
+      </Modal>
+    </View>
+  );
 }
 
 export default function App() {
@@ -61,11 +63,11 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SavedPasswords" component={SavedPasswords} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -81,26 +83,25 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "bold",
     marginBottom: 20,
-
+    color: "#A62F24"
   },
   button: {
-    backgroundColor: "#333",
+    backgroundColor: "#A62F24",
     width: '70%',
     height: 50,
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 20
+    marginBottom: 20,
   },
   textButton: {
     color: "#FFF",
     fontWeight: "bold",
     fontSize: 18,
-
   },
   genText: {
     fontSize: 16,
     fontWeight: "bold",
     color: "#333",
-  }
+  },
 });
